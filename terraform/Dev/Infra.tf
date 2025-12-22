f72c966a30f0436a3bf6c773b0a7cd5919e08206
Mudit Nagpal <mudit_nagpal@persistent.com>
2025-12-22 22:36:21 +0530

    tf infra file

--- terraform/Dev/Infra.tf
+++ terraform/Dev/Infra.tf
@@ -15,18 +15,6 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-# DynamoDB table for Terraform state locking
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "dev-tf-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-
 module "s3" {
   source = "../../terraform-module/s3"
   bucket_name = "demobuck-tfs-dev"
